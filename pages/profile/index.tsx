7b8788f57dc15d860afc2b108dbc87ad7c19d6bd
sangmoon <laugh120@naver.com>
2023-01-20 19:35:11 +0900

    ui-complete

---
+++ pages/profile/index.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import type { NextPage } from "next";
+
+const Page: NextPage = () => {
+  return (
+      <Layout prevPage title="내 정보">
+        <div>
+          내화면이요
+        </div>
+      </Layout>
+    );
+};
+
+export default Page;
